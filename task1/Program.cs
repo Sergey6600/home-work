11c918b8b2cb184c51854c67727619b627370ba1
serega.6600 <serega.6600@yandex.ru>
2022-12-21 00:14:18 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine(" введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int maxNumber = number1;
+if( number2 > maxNumber){
+ maxNumber = number2;
+}
+if( number3 > maxNumber){
+ maxNumber = number3;
+}
+System.Console.WriteLine(" MAX:" + maxNumber);
