e66b389ef1b824cccc1ffbb23ffff4421bd65655
serega.6600 <serega.6600@yandex.ru>
2022-12-21 00:07:44 +0300

    решили задачу

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число:"); 
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int maxNumber = 0; 
+int minNumber = 0;
+if (number1 > number2)
+{
+   maxNumber = number1; 
+   minNumber = number2;
+   Console.Write("Максимальное число : " + maxNumber, minNumber); 
+}
+else if (number1 == number2)
+{
+   Console.WriteLine("Числа равны");
+}
+else
+{ 
+   maxNumber = number2;
+   minNumber = number1;
+   Console.WriteLine("Максимальное число : " + maxNumber, minNumber);
+}
