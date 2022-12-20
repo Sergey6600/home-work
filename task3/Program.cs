142e3f953595ae7b4c03004428b08960596db5f2
serega.6600 <serega.6600@yandex.ru>
2022-12-21 00:29:39 +0300

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число, большее 1:");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    if (n <= 1) Console.WriteLine("Число меньше 1");
+    else {
+        int evenNumber = 2;
+        while (evenNumber <= n-2) {
+            Console.Write(evenNumber + ", ");
+            evenNumber += 2;
+        }
+        Console.WriteLine(evenNumber);
+    }
+
+    Console.WriteLine("Нажмите Enter для продолжения");
+    Console.ReadLine();
