3aa335cc1e18ae55054005117aa5c9e12f3b2127
serega.6600 <serega.6600@yandex.ru>
2022-12-22 22:50:29 +0300

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,20 @@
+﻿ Console.Write("Введите число: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+ int res = num;
+ int i = 0;
+
+ if (num > 99 || num < -99) {
+     while (num != 0)
+     {
+         num /= 10;
+         i++;
+     }
+     while (i > 3)
+     {
+         res /= 10;
+         i--;
+     }
+     res %= 10;
+     Console.WriteLine(res);
+ }
+ 
