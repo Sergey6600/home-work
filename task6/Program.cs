7989f960b697263c32d263b66f2fa96b12a80b12
serega.6600 <serega.6600@yandex.ru>
2022-12-22 23:02:59 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Напишите цифру соответствующую дню недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Ошибка");
+}
+
+else
+{
+    if (number >= 6 || number <= 7)
+    {
+    Console.WriteLine("Является выходным");
+    }
+    else if (number < 6)
+    {
+    Console.WriteLine("Не является выходным");
+    }
+}
