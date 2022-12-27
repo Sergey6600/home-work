e526772712995d297150af9c048a5eb5e3b46197
serega.6600 <serega.6600@yandex.ru>
2022-12-28 00:18:31 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число, большее 1:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 1) Console.WriteLine("Число меньше 1");
+else
+ {
+int cube = 1;
+for (cube = 1; cube < n; cube++) {
+Console.Write(Math.Pow(cube, 3) + ", ");
+}
+Console.WriteLine(Math.Pow(cube, 3));
+}
+Console.WriteLine("Нажмите Enter для продолжения");
+Console.ReadLine();
