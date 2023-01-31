ab0195dff14d013e609a54ad8e7a1be5599dde81
serega.6600 <serega.6600@yandex.ru>
2023-01-31 21:51:21 +0300

    task 30

--- task30/Program.cs
+++ task30/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число M:")
+== Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ") ; 
+Convert.ToInt32(Console.ReadLine());
+SumFromMToN(m, n);
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN (int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
